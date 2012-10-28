c5c6ccdd27a2706d15723134925d15b0f3cb3bde
rbucks <rbucks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-28 17:32:44 +0000

    Tagging v 1.1
    git-svn-id: https://plugins.svn.wordpress.org/scripted-api/tags/1.1@618332 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/settings.php
@@ -0,0 +1,101 @@
+<?php
+add_action('admin_menu', 'scripted_settings_menu');
+add_action( 'admin_notices', 'scripted_install_warning' );
+
+/**
+ * Display an admin-facing warning if the current user hasn't authenticated with scripted yet
+ *
+ * @since 1.0
+ */
+function scripted_install_warning() {
+	$apiKey                  = get_option( '_scripted_api_key' );
+        $_scripted_business_id   = get_option( '_scripted_business_id' );
+
+	$page = (isset($_GET['page']) ? $_GET['page'] : null);
+
+	if ((empty($apiKey)  || empty($_scripted_business_id)) && $page != 'scripted_settings_menu' && current_user_can( 'manage_options' ) ) {
+		admin_dialog( sprintf( 'You must %sconfigure the plugin%s to enable Scripted for WordPress.', '<a href="admin.php?page=scripted_settings_menu">', '</a>' ), true);
+	}
+}
+function admin_dialog($message, $error = false) {
+	if ($error) {
+		$class = 'error';
+	}
+	else {
+		$class = 'updated';
+	}
+	
+	echo '<div ' . ( $error ? 'id="scripted_warning" ' : '') . 'class="' . $class . ' fade' . '"><p>'. $message . '</p></div>';
+}
+function scripted_settings_menu() {
+   add_menu_page('scripted_settings', 'Settings', 'add_users','scripted_settings_menu', 'scripted_settings_menu_function', SCRIPTED_ICON, 83);
+   
+   $apiKey                  = get_option( '_scripted_api_key' );
+   $_scripted_business_id  = get_option( '_scripted_business_id' );
+    
+    if($apiKey != '' and $_scripted_business_id !='') {
+	$createAJobPage = add_submenu_page( 'scripted_settings_menu', 'Create a Job', 'Create a Job', 'manage_options', 'scripted_create_a_job', 'scripted_create_a_job_callback' ); 
+        add_action( 'admin_footer-'. $createAJobPage, 'getFormFields' );
+        add_submenu_page( 'scripted_settings_menu', 'Current Jobs', 'Current Jobs', 'manage_options', 'scripted_create_current_jobs', 'scripted_create_current_jobs_callback' );
+        $finishedPage = add_submenu_page( 'scripted_settings_menu', 'Finished Jobs', 'Finished Jobs', 'manage_options', 'scripted_create_finished_jobs', 'scripted_create_finished_jobs_callback' );
+        add_action( 'admin_footer-'. $finishedPage, 'createProjectAjax' );
+    }
+}
+function scripted_settings_menu_function() {
+    
+  if(isset($_POST) && wp_verify_nonce($_POST['_wpnonce'],'scriptedFormAuthSettings')) {        
+      
+      $validate = validateApiKey($_POST['_scripted_api_key'],$_POST['_scripted_business_id']);
+        if($validate) {
+            update_option( '_scripted_api_key', sanitize_text_field($_POST['_scripted_api_key']) );        
+            update_option( '_scripted_business_id', sanitize_text_field($_POST['_scripted_business_id'] ));        
+        } else {
+            echo '<div class="updated" id="message"><p>Sorry, we found an error. Please confirm your API key and Business ID are correct and try again.</p></div>';
+        }
+    }
+   $out = '<div class="wrap">
+            <div class="icon32" style="width:100px;padding-top:5px;" id="icon-scripted"><img src="'.SCRIPTED_LOGO.'"></div><h2>Settings</h2>';
+   
+   $out .='<p>Authentication is required for many functions of the Scripted API. We use token-based authentication.<br />
+You can think of your business_id as your username, and your key as your password.</p>';
+   
+   $out .='<p>To get your Business ID and key, please register or log in at Scripted.com, and go to https://Scripted.com/api. Your credentials will show at the top of this page.</p>';
+            
+   $out .='<form action="" method="post" name="scripted_settings">'.wp_nonce_field( 'scriptedFormAuthSettings', '_wpnonce' );
+   
+   $apiKey                  = get_option( '_scripted_api_key' );
+   $_scripted_business_id   = get_option( '_scripted_business_id' );
+   
+   $out .='<table class="form-table">
+      <tbody>
+        <tr valign="top">
+          <th scope="row"><label for="api_key">API Key</label></th>
+          <td><input type="text" class="regular-text" value="'.$apiKey.'" id="_scripted_api_key" name="_scripted_api_key"></td>
+        </tr>
+        <tr valign="top">
+          <th scope="row"><label for="business_id">Business Id</label></th>
+          <td><input type="text" class="regular-text" value="'.$_scripted_business_id.'" id="_scripted_business_id" name="_scripted_business_id"></td>
+        </tr>
+     </tbody>
+    </table>
+    <p class="submit">
+      <input type="submit" value="Save Changes" class="button-primary" id="submit" name="submit">
+    </p>';
+   
+   
+   $out .='</form>';
+   
+   $out .='</div>';// end of wrap div
+   echo $out;
+}
+function validateApiKey($apiKey,$businessId)
+{
+   $_currentJobs = @file_get_contents('https://scripted.com/jobs?key='.$apiKey.'&business_id='.$businessId); 
+   if($_currentJobs != '') {
+        $_currentJobs = json_decode($_currentJobs);
+        if(isset($_currentJobs->total)) {
+            return true;
+        }
+   }
+   return false;
+}
---
+++ trunk/readme.txt
@@ -0,0 +1,40 @@
+=== Scripted.com Expert Blogger API ===
+Contributors: rbucks
+Donate link: 
+Tags: writing, blog posts, twitter, tweet, hire blogger, hire writer, custom content, scripted.com, expert writer, scripted
+Requires at least: 3.3
+Tested up to: 3.4
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Get your blog posts written by our pre-screened network of top notch writers. Scripted makes it easy to order consistently great blog posts that you own 100% of the rights to publish.
+
+== Description ==
+
+Scripted helps small and large businesses write expert marketing content. We attract and screen the best American freelance writers with expertise ranging from healthcare to hardware. We offer a satisfaction guarantee and flat rate pricing on each of our formats, which include blog posts, white papers, tweets, press releases, and website copy. For high volume customers, we offer full account support and a robust API to post jobs and pull content automatically. Past clients include Levi's, Viacom, and VeriSign, and numerous startups in the Bay Area and around the world.
+
+== Installation ==
+
+1. Upload the `scripted` folder to the `/wp-content/plugins/` directory .
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Create an account at Scripted.com by going here: https://scripted.com/businesses/sign_up
+4. View and copy your API key and business ID here: https://scripted.com/api
+5. Go to your Scripted API settings (see screenshot) and enter it in the Scripted settings page. 
+
+== Frequently asked questions ==
+
+See our frequently asked questions here: [https://Scripted.com/faq](https://Scripted.com/faq)
+
+== Screenshots ==
+
+1. Scripted API settings
+2. Create a Job
+3. View Current Jobs
+4. View Finished Jobs and Create Posts
+
+== Changelog ==
+
+
+
+== Upgrade notice ==
