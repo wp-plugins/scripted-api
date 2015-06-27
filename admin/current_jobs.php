cca3c8db315e1add2d598a9b80765b11514cf2d1
rbucks <rbucks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-27 04:10:52 +0000

    tagging version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/scripted-api/tags/1.4@1188447 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/current_jobs.php
+++ admin/current_jobs.php
@@ -2,124 +2,104 @@
 
 function scripted_create_current_jobs_callback()
 {
-    $apiKey                 = get_option( '_scripted_api_key' );
-    $_scripted_business_id  = get_option( '_scripted_business_id' );
-    $paged                  = (isset($_GET['paged']) and $_GET['paged'] !='') ? $_GET['paged'] : 1;
-    $per_page               = 10;
+    wp_enqueue_style('thickbox');
+    wp_enqueue_script('thickbox');    
     
-    $validate = validateApiKey($apiKey,$_scripted_business_id);
-    
+    $ID               = get_option( '_scripted_ID' );
+    $accessToken      = get_option( '_scripted_auccess_tokent' );
+    $paged            = (isset($_GET['paged']) and $_GET['paged'] !='') ? sanitize_text_field($_GET['paged']) : '';
+    $per_page         = 15;    
+    $validate = validateApiKey($ID,$accessToken);    
     $out = '<div class="wrap">
-            <div class="icon32" style="width:100px;padding-top:5px;" id="icon-scripted"><img src="'.SCRIPTED_LOGO.'"></div><h2>Current Jobs <a class="add-new-h2" href="admin.php?page=scripted_create_a_job">Create a Job</a></h2>';
+            <div class="icon32" style="width:100px;padding-top:5px;" id="icon-scripted"><img src="'.SCRIPTED_LOGO.'"></div><h2>Jobs</h2>';
     
     if($validate) {
-        $_currentJobs = @file_get_contents('https://scripted.com/jobs?key='.$apiKey.'&business_id='.$_scripted_business_id.'&page='.$paged.'&per_page='.$per_page.'&');            
-        $_currentJobs = json_decode($_currentJobs);
+        $url = ($paged != '') ? 'jobs?next_cursor='.$paged : 'jobs/';
+        $result = curlRequest($url);
         
-        $totalPagess  = $_currentJobs->total;
-        $totalPages = ceil($totalProjects/$per_page);
+        $allJobs = $result->data; 
+        
+        $next = (isset($result->paging->has_next) and $result->paging->has_next == 1) ? $result->paging->next_cursor : '';
+        $totalProjects  = $result->total_count;
+        $totalPages     = ceil($totalProjects/$per_page);
         
         // paggination
+        
+        $paggination = '';
+        
          $pageOne = '';         
-         if($totalPages < 2)
+         if($paged == '' and $result->paging->has_next != 1)
              $pageOne = ' one-page';     
          
-         $out .='<div class="tablenav top">
+         $paggination .='<div class="tablenav">
             <div class="tablenav-pages'.$pageOne.'">';
          
-                $out .='<span class="displaying-num">'.$totalProjects.' items</span>';
-                $prePage = '';
-                if($paged < 2) 
-                    $prePage = 'disabled';
+                $paggination .='';
                 $nextPage = '';
-                if($totalPages == $paged) 
+                if($result->paging->has_next != 1) 
                     $nextPage = 'disabled';
                 
-                $preLink = 1;
-                if($paged > 1) 
-                    $preLink = $paged-1;
-                $nextLink = $totalPages;
-                if($paged < $totalPages) 
-                    $nextLink = $paged+1;
-                
-                $out .='<span class="pagination-links"><a href="admin.php?page=scripted_create_current_jobs&paged=1" title="Go to the first page" class="first-page  '.$prePage.'">&laquo;</a>
-                        <a href="admin.php?page=scripted_create_current_jobs&paged='.$preLink.'" title="Go to the previous page" class="prev-page '.$prePage.'">&lsaquo;</a> 
-                            <span class="paging-input">'.$paged.' of <span class="total-pages">'.$totalPages.'</span></span>
-                            <a href="admin.php?page=scripted_create_current_jobs&paged='.$nextLink.'" title="Go to the next page" class="next-page '.$nextPage.'">&rsaquo;</a>
-                        <a href="admin.php?page=scripted_create_current_jobs&paged='.$totalPages.'" title="Go to the last page" class="last-page '.$nextPage.'">&raquo;</a>';
+                $paggination .='<span class="pagination-links"> 
+                            <span class="displaying-num">'.$totalProjects.' items</span>
+                            <a href="admin.php?page=scripted_create_current_jobs&paged='.$next.'" title="Go to the next page" class="next-page '.$nextPage.'">&rsaquo;</a>';
          
-                   $out .='</span>
+                   $paggination .='</span>
              </div>
             <br class="clear">
             </div>';
         // paggination end
+                   
+        $out .= $paggination;
         
-        $out .='<table cellspacing="0" class="wp-list-table widefat fixed pages">
+        $out .='<table cellspacing="0" class="wp-list-table widefat sTable">
                     <thead>
                         <tr>
-                        <th style="" class="manage-column column-author"scope="col"><span>Topic</span></th>
-                        <th style="" class="manage-column column-author"scope="col"><span>State</span></th>
-                        <th style="" class="manage-column column-author"scope="col"><span>Deadline</span></th>
+                        <th scope="col" width="40%"><span>Topic</span></th>
+                        <th scope="col" width="10%"><span>Quantity</span></th>
+                        <th scope="col" width="10%"><span>State</span></th>
+                        <th scope="col" width="15%"><span>Deadline</span></th>
+                        <th scope="col" width="23%"></th>
                         </tr>
                     </thead>
-                      <tbody id="the-list">
-                    ';
+                      <tbody>';
         
-        if($_currentJobs->total) {
-            $currentJobs = $_currentJobs->jobs;
-            
-            foreach($currentJobs as $job) {
+        if($allJobs)  {           
+            $i = 1;
+            foreach($allJobs as $job) {
                 $out .='<tr valign="top" class="scripted type-page status-publish hentry alternate">
-                    <td class="author column-author"><strong>'.$job->topic.'</strong></td>
-                    <td class="author column-author">'.$job->state.'</td>
-                    <td class="author column-author">'.date('F j', strtotime($job->deadline_at)).'</td>
-                    </tr>';
+                    <input type="hidden" id="project_'.$i.'" value="'.$job->id.'">
+                    <td>'.$job->topic.'</td>
+                    <td>'.$job->quantity.'</td>
+                    <td>'.ucfirst($job->state).'</td>
+                    <td>'.date('F j', strtotime($job->deadline_at)).'</td>';
+                
+                    $out .='<td>';
+                    if($job->state == 'ready for review') {
+                        $out .= '<a id="accept_'.$job->id.'"  href="javascript:void(0)" onclick="finishedProjectActions(\''.$job->id.'\',\'Accept\')">Accept</a> | ';
+                        $out .= '<a id="request_'.$job->id.'"  href="'.admin_url('admin-ajax.php').'?action=scripted_poject_finished&do=request_edit&project_id='.$job->id.'&secure='.wp_create_nonce('request_edit').'&amp;type=page&amp;TB_iframe=1&amp;width=600&amp;height=400" class="thickbox" title="'.strip_tags(substr($job->topic,0,50)).'">Request Edits</a>';
+                    }elseif($job->state == 'ready for acceptance') {
+                        $out .= '<a id="accept_'.$job->id.'"  href="javascript:void(0)" onclick="finishedProjectActions(\''.$job->id.'\',\'Accept\')">Accept</a> | ';
+                        $out .= '<a id="reject_'.$job->id.'"  href="javascript:void(0)" onclick="finishedProjectActions(\''.$job->id.'\',\'Reject\')">Reject</a>';
+                    }elseif ($job->state == 'accepted') {
+                        $out .= '<a id="create_'.$job->id.'" href="javascript:void(0)"  onclick="finishedProjectActions(\''.$job->id.'\',\'Create\')">Create Draft</a> | ';
+                        $out .= '<a id="post_'.$job->id.'" href="javascript:void(0)"  onclick="finishedProjectActions(\''.$job->id.'\',\'Post\')">Create Post</a> | ';
+                        $out .= '<a href="'.admin_url('admin-ajax.php').'?action=scripted_poject_finished&do=view_project&project_id='.$job->id.'&secure='.wp_create_nonce('view_project').'&amp;type=page&amp;TB_iframe=1&amp;width=850&amp;height=500" class="thickbox" title="'.strip_tags(substr($job->topic,0,50)).'">View</a>';
+                    }
+                    $out .='</td>';
+                    $out .='</tr>';
+                    $i++;
             }
             
         } else {
-            $out .='<tr valign="top" class="scripted type-page status-publish hentry alternate">
-                    <th colspan="4"  style="text-align:center;" class="check-column"><strong>Your Scripted account has no Current Jobs. <a href="admin.php?page=scripted_create_a_job">Create a Job</a></strong></td>
+            $out .='<tr valign="top">
+                    <th colspan="5"  style="text-align:center;" class="check-column"><strong>Your Scripted account has no Current Jobs. <a href="admin.php?page=scripted_create_a_job">Create a Job</a></strong></td>
                     </tr>';
         }
         
          $out .= '</tbody>
-                </table>
-                '; // end table
+                </table>'; // end table
         
-       // paggination
-         $pageOne = '';         
-         if($totalPages < 2)
-             $pageOne = ' one-page';     
-         
-         $out .='<div class="tablenav bottom">
-            <div class="tablenav-pages'.$pageOne.'">';
-         
-                $out .='<span class="displaying-num">'.$totalProjects.' items</span>';
-                $prePage = '';
-                if($paged < 2) 
-                    $prePage = 'disabled';
-                $nextPage = '';
-                if($totalPages == $paged) 
-                    $nextPage = 'disabled';
-                
-                $preLink = 1;
-                if($paged > 1) 
-                    $preLink = $paged-1;
-                $nextLink = $totalPages;
-                if($paged < $totalPages) 
-                    $nextLink = $paged+1;
-                
-                $out .='<span class="pagination-links"><a href="admin.php?page=scripted_create_current_jobs&paged=1" title="Go to the first page" class="first-page  '.$prePage.'">&laquo;</a>
-                        <a href="admin.php?page=scripted_create_current_jobs&paged='.$preLink.'" title="Go to the previous page" class="prev-page '.$prePage.'">&lsaquo;</a> 
-                            <span class="paging-input">'.$paged.' of <span class="total-pages">'.$totalPages.'</span></span>
-                            <a href="admin.php?page=scripted_create_current_jobs&paged='.$nextLink.'" title="Go to the next page" class="next-page '.$nextPage.'">&rsaquo;</a>
-                        <a href="admin.php?page=scripted_create_current_jobs&paged='.$totalPages.'" title="Go to the last page" class="last-page '.$nextPage.'">&raquo;</a>';
-         
-                   $out .='</span>
-             </div>
-            <br class="clear">
-            </div>';
-        // paggination end
+       $out .= $paggination;
          
          
     }
