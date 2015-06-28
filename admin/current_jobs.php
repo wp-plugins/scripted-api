bcabb36834f105a1211f569b7c39e0d0db905665
rbucks <rbucks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-28 05:18:32 +0000

    tagging version 1.5.2
    git-svn-id: https://plugins.svn.wordpress.org/scripted-api/trunk@1188844 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/current_jobs.php
+++ admin/current_jobs.php
@@ -108,3 +108,160 @@ function scripted_create_current_jobs_callback()
     
     echo $out;
 }
+function createScriptedProject($proId,$ID,$accessToken,$type = 'draft')
+{
+    global $current_user;
+    $userID = $current_user->ID;
+    
+    $_projectJob = curlRequest('jobs/'.$proId);
+    $_projectContent = curlRequest('jobs/'.$proId.'/html_contents');
+    if($_projectContent->id == $proId and !empty($_projectJob)) {
+        $content = $_projectContent->html_contents;
+        if(is_array($content)) {
+            $content = $content[0];
+        }
+        $success_message = 'Draft Created!';
+        $post['post_title']     = wp_strip_all_tags($_projectJob->topic);
+        if($type == 'draft')
+            $post['post_status']    = 'draft';
+        elseif($type == 'publish') {
+            $post['post_status']    = 'publish';
+            $success_message = 'Post Published!';
+        }
+        $post['post_author']    = $userID;
+        $post['post_type']      = 'post';
+        $post['post_content']   = $content;
+        $post['post_content']  .= '<p style="font-style:italic; font-size: 10px;">Powered by <a href="https://app.scripted.com" alt="Scripted.com content marketing automation">Scripted.com</a></p>';
+        $post_id                = wp_insert_post($post ,true); // draft created
+        echo $success_message;
+        $track_url = 'http://toofr.com/api/track?url='.urlencode(get_permalink($post_id)).'&title='.urlencode($post['post_title']);
+        @file_get_contents($track_url);
+    } else {
+        echo 'Failed';
+    }
+        
+   
+}
+function createProjectAjax()
+{
+    ?>
+    <script>
+       function finishedProjectActions(proId,actions) {
+           if(actions == 'Accept')
+                jQuery("#accept_"+proId).html('Accepting...'); 
+           else if(actions == 'Reject')
+                jQuery("#reject_"+proId).html('Rejecting...'); 
+           else if(actions == 'Create')
+                jQuery("#create_"+proId).html('Creating...'); 
+           else if(actions == 'Post')
+                jQuery("#post_"+proId).html('Creating Post...'); 
+                
+            jQuery.ajax({
+                    type: 'POST',
+                    url: '<?php echo wp_nonce_url( admin_url('admin-ajax.php'), 'create_reject_accept' );?>&do='+actions+'&project_id='+proId+'&action=scripted_poject_finished',
+                    data: '',
+                    success: function(data) {                            
+                        if(actions == 'Accept')
+                            jQuery("#accept_"+proId).html(data); 
+                       else if(actions == 'Reject')
+                            jQuery("#reject_"+proId).html(data); 
+                       else if(actions == 'Create')
+                            jQuery("#create_"+proId).html(data); 
+                       else if(actions == 'Post')
+                            jQuery("#post_"+proId).html(data); 
+                    }
+                });
+       }
+       function doSorting() {
+           var sortDo =  jQuery("#actions").val(); 
+           if(sortDo == '')
+                document.location.href='<?php echo admin_url();?>/admin.php?page=scripted_create_finished_jobs<?php echo (isset($_GET['paged']) and $_GET['paged'] !='') ? '&paged'.$_GET['paged'] : ''?>';
+           else
+               document.location.href='<?php echo admin_url();?>/admin.php?page=scripted_create_finished_jobs<?php echo (isset($_GET['paged']) and $_GET['paged'] !='') ? '&paged='.$_GET['paged'] : ''?>&sort='+sortDo;
+       }
+       function completeActionRefresh() {
+           window.location.reload();
+       }
+    </script>
+        <?php
+}
+add_action('wp_ajax_scripted_poject_finished', 'scriptedPojectFinished');
+function scriptedPojectFinished() {
+    $do             = (isset($_GET['do']) and $_GET['do'] !='') ? sanitize_text_field($_GET['do']) : '';
+    $project_id     = (isset($_GET['project_id']) and $_GET['project_id'] !='') ? sanitize_text_field($_GET['project_id']) : '';
+    
+    $ID               = get_option( '_scripted_ID' );
+    $accessToken      = get_option( '_scripted_auccess_tokent' );
+    $validate               = validateApiKey($ID,$accessToken);
+    
+    $scriptedBaseUrl        = 'https://app.scripted.com/';
+    
+    if(!$validate or $project_id == '' or $do == '') 
+        die('Failed');
+    
+    if(wp_verify_nonce($_GET['secure'],'view_project') and $do == 'view_project') {
+        $_projectContent = curlRequest('jobs/'.$project_id.'/html_contents');
+        
+        if($_projectContent->id == $project_id) {
+            $content = $_projectContent->html_contents;
+            if(is_array($content)) {
+                $content = $content[0];
+            }            
+            echo $content;
+        }
+    }elseif(wp_verify_nonce($_GET['_wpnonce'],'create_reject_accept') and $do == 'Accept') {
+        $_projectAction = curlRequest('jobs/'.$project_id.'/accept',true);
+        if($_projectAction)
+            echo 'Accepted';
+        else
+            echo 'Failed';
+    }elseif(wp_verify_nonce($_GET['_wpnonce'],'create_reject_accept') and $do == 'Reject') {
+        $_projectAction = curlRequest('jobs/'.$project_id.'/reject',true);     
+        if($_projectAction)
+            echo 'Accepted';
+        else
+            echo 'Failed';
+    }elseif(wp_verify_nonce($_GET['_wpnonce'],'create_reject_accept') and $do == 'Create') {
+        createScriptedProject($project_id,$ID,$accessToken);
+    }elseif(wp_verify_nonce($_GET['_wpnonce'],'create_reject_accept') and $do == 'Post') {
+        createScriptedProject($project_id,$ID,$accessToken,'publish');
+    }elseif(wp_verify_nonce($_GET['secure'],'request_edit') and $do == 'request_edit') {
+        
+        if(empty($_POST))
+            getFormRequestEditProject($project_id);
+        else {
+            $chief_complaint = $_POST['chief_complaint'];
+            $_projectAction = curlRequest('jobs/'.$project_id.'/request_edits',true,'feedback='.$chief_complaint); 
+            
+            if($_projectAction)
+                echo 'Accepted';
+            else
+                echo 'Failed';
+            
+            echo '<script type="text/javascript">';
+            echo 'window.top.completeActionRefresh();';
+            echo '</script>';
+        }
+    }
+    die();
+}
+function getFormRequestEditProject() {
+    
+    $out ='<form action="" method="post" name="frmEditRequests" id="frmEditRequests" onsubmit="return sendEditRequest();">'.wp_nonce_field( 'edit_requests', '_wpnonce' );
+    $out .= '<label for="chief_complaint">Chief Complaint</label></br></br>';
+    $out .= '<textarea id="chief_complaint" name="chief_complaint" style="width:400px; height:200px;"></textarea></br>';
+    $out .='<input type="submit" value="Request Edits" class="button-primary" name="submit">';
+    $out .='</form>';
+    $out .='<script>';
+    $out .='function sendEditRequest() {
+                var chief_complaint = document.getElementById("chief_complaint").value;
+                if(chief_complaint == "") {
+                    document.getElementById("chief_complaint").style.border="1px solid red";
+                    return false;
+                }                
+                return true;
+            }
+        ';
+    $out .='</script>';
+    echo $out;
+}
